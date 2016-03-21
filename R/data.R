fd443d0d2ad97a3201ad6081e7bd6e746a89fd22
Nam Pho <nampho@me.com>
2016-03-21 10:49:41 -0300

    Add support for documented NHANES data set.

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' NHANES survey results.
+#'
+#' A dataset containing the NHANES survey information.
+#'
+#' @docType data
+#'
+#' @usage data(nhanes)
+#' 
+#' @format A data frame with 10122 rows and 939 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#'
+#' @source \url{http://www.nhanes.org/}
+#'
+#' @keywords datasets
+#'
+#' @examples data(nhanes)
+"nhanes"
